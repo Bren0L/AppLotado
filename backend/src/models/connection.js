8f8ef26f74a1e4ab6c16d812dbfe0854d12bcfee
Bren0L <brenoleandr@gmail.com>
2023-05-22 15:48:55 -0300

    aplication

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,11 +1,22 @@
-const mysql = require("mysql2/promise");
+const firebase = require("fire");
 const dotenv = require("dotenv").config();
 
-const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyCAk39jqSMuPWt11qZqihOu7yw4lPccP3E",
+    authDomain: "onibus-fb557.firebaseapp.com",
+    databaseURL: "https://onibus-fb557-default-rtdb.firebaseio.com",
+    projectId: "onibus-fb557",
+    storageBucket: "onibus-fb557.appspot.com",
+    messagingSenderId: "830589986262",
+    appId: "1:830589986262:web:f4d4b0a25e50180f8bb077",
+    measurementId: "G-N7ST7SGZM0"
+  };
 
-module.exports = connection;
+const app = firebase.initializeApp(firebaseConfig);
+const realtimeDatabase = app.database();
+const auth = app.auth();
+
+module.exports = {
+    realtimeDatabase,
+    auth
+};
